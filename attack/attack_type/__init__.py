b59cd4959f198b7f28f6ca5a1d95b33e48701809
幼月冥 <3374246447@qq.com>
2024-04-27 17:01:39 +0800

    删除旧日蚀攻击(保留文件更名为alpha版本不对外开放)，增加两个新的日蚀攻击

--- attack/attack_type/__init__.py
+++ attack/attack_type/__init__.py
@@ -2,6 +2,6 @@ from .attack_type import AttackType
 from .honest_mining import HonestMining
 from .double_spending import DoubleSpending
 from .selfish_mining import SelfishMining
-from .eclipse import Eclipse
+from .eclipse_alpha import Eclipse
+from .eclipse_network_cutting import EclipseNetworkCutting
 from .eclipsed_double_spending import EclipsedDoubleSpending
-from .eclipse_demo import EclipseNetworkCutting
