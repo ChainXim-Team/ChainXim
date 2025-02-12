fad153ea03b4a8f91dd3ab320fa2725f570ef544
czxdev <36617515+czxdev@users.noreply.github.com>
2025-02-12 20:11:22 +0800

    fix: random_oracle's python311/python312 支持

--- consensus/__init__.py
+++ consensus/__init__.py
@@ -5,5 +5,4 @@ try:
     from .random_oracle import RandomOracleRoot, RandomOracleMining, RandomOracleVerifying
     from .powstrict import PoWstrict
 except ImportError:
-    print('''Warning: module random_oracle and PoWstrict are not avaiable.
-         Please make sure you are using python 3.10.''')
+    print('''Warning: fail to import module random_oracle. PoWstrict is not avaiable.''')
