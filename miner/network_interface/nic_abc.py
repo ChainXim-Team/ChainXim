8a1965e645ae447f01cc9468eae346409313480f
cbw <2529091077@qq.com>
2025-02-27 17:20:41 +0800

    fix:攻击者一轮可发多个消息

--- miner/network_interface/nic_abc.py
+++ miner/network_interface/nic_abc.py
@@ -39,7 +39,8 @@ class NetworkInterface(metaclass=ABCMeta):
             if self._network.withTopology and syncLocalChain:
                 msg = SYNC_LOC_CHAIN
             self._forward_buffer[SELF_GEN_MSG].append([msg, forward_strategy, spec_target])
-            if not self._network.withTopology and len(self._forward_buffer[SELF_GEN_MSG]) > 1:
+            if (not self._network.withTopology and len(self._forward_buffer[SELF_GEN_MSG]) > 1 
+                and not self.miner.isAdversary):
                 raise ValueError("Each round, a miner can only put one message into the network.")
         elif msg_source_type == OUTER_RCV_MSG:
             self._forward_buffer[OUTER_RCV_MSG].append([msg, forward_strategy, spec_target])
