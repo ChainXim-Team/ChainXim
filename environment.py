4c3162ec54c854473dd36b012052037c7e3f3e3e
幼月冥 <3374246447@qq.com>
2025-03-14 15:35:09 +0800

    修改双花攻击成功次数判断

--- environment.py
+++ environment.py
@@ -469,22 +469,27 @@ class Environment(object):
         if self.adversary.get_info():
             print('The simulation data of', self.adversary.get_attack_type_name() , 'is as follows', ':\n', self.adversary.get_info())
             stats.update(self.adversary.get_info())
-            print('Double spending success times:', stats["double_spending_success_times"])
-            print('Double spending success times (ver2):', stats["double_spending_success_times_ver2"])
+
             if self.adversary.get_eclipsed_ids() is not None:
-                honest = set(self.honest_miner_ids) - set(self.adversary.get_eclipsed_ids())
-                last_block = self.global_chain.get_last_block()
-                attack_flag = False
-                success_times = 0
-                while last_block.parentblock:
-                    if last_block.blockhead.miner not in honest and not attack_flag:
-                        attack_flag = True
-                    last_block = last_block.parentblock
-                    if len(last_block.next) > 1 and attack_flag:
-                        attack_flag = False
-                        success_times += 1
-                    
-                print('Double spending success times (eclipsed):', success_times)
+                eclipesd_ids = self.adversary.get_eclipsed_ids()
+            else:
+                eclipesd_ids = []
+            mainchain_block = set()
+            double_spending_success_times = 0
+            current_block = self.global_chain.last_block
+            while current_block:
+                mainchain_block.add(current_block.name)
+                current_block = current_block.parentblock
+            last_block = self.global_chain.last_block.parentblock
+            while last_block:
+                if len(last_block.next) > 1:
+                    for block in last_block.next:
+                        if (block.isAdversaryBlock or block.blockhead.miner in eclipesd_ids) and block.name in mainchain_block:
+                            double_spending_success_times += 1
+                            break
+                last_block = last_block.parentblock
+            print('Double spending success times:', double_spending_success_times)
+            
         # Network Property
         if not isinstance(self.network,network.SynchronousNetwork):
             print('Block propagation times:', ave_block_propagation_times)
